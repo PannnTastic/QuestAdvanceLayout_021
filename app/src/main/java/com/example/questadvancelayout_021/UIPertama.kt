19f1fd21c3c1d3f4a692a19a381706c1611dad4a
PannnTastic <pannntastic@gmail.com>
2025-10-23 16:08:53 +0700

    add box below card

--- app/src/main/java/com/example/questadvancelayout_021/UIPertama.kt
+++ app/src/main/java/com/example/questadvancelayout_021/UIPertama.kt
@@ -1,6 +1,7 @@
 package com.example.questadvancelayout_021
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -67,6 +68,11 @@ fun AktivitasPertama(){
                     )
                 }
             }
+        }
+        Box(
+            modifier = Modifier.fillMaxSize()
+        ){
+
         }
     }
 }
