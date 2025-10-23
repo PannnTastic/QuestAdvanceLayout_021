47c57be26a1a38a8767fd6dfa37cba47337617bd
PannnTastic <pannntastic@gmail.com>
2025-10-23 15:53:08 +0700

    Add first Card

--- app/src/main/java/com/example/questadvancelayout_021/UIPertama.kt
+++ app/src/main/java/com/example/questadvancelayout_021/UIPertama.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -26,5 +27,6 @@ fun AktivitasPertama(){
         Text(text = stringResource(id = R.string.univ),
             fontSize = 22.sp)
         Spacer(modifier = Modifier.height(height = 25.dp))
+        Card {  }
     }
 }
