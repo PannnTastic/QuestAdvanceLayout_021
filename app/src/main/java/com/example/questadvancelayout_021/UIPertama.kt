169df06b7063f58d735ab715606583ee0ee5f7ba
PannnTastic <pannntastic@gmail.com>
2025-10-23 15:51:01 +0700

    Add spacer under univ string

--- app/src/main/java/com/example/questadvancelayout_021/UIPertama.kt
+++ app/src/main/java/com/example/questadvancelayout_021/UIPertama.kt
@@ -1,7 +1,9 @@
 package com.example.questadvancelayout_021
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -23,5 +25,6 @@ fun AktivitasPertama(){
             fontWeight = FontWeight.Bold)
         Text(text = stringResource(id = R.string.univ),
             fontSize = 22.sp)
+        Spacer(modifier = Modifier.height(height = 25.dp))
     }
 }
