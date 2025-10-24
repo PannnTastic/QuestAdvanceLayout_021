ec6bcbb00044a965f13341e2cf341a5ebcf98116
PannnTastic <pannntastic@gmail.com>
2025-10-24 14:47:00 +0700

    gradlenya cari masalah

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,9 +6,7 @@ plugins {
 
 android {
     namespace = "com.example.questadvancelayout_021"
-    compileSdk {
-        version = release(36)
-    }
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.example.questadvancelayout_021"
